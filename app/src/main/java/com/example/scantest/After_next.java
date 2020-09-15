84366d2d0bff4fcabd4e48b37449c24c125767de
soumen <palsoumen306@gmail.com>
2020-09-16 01:28:06 +0530

    crop in obutton nev ber

--- app/src/main/java/com/example/scantest/After_next.java
+++ app/src/main/java/com/example/scantest/After_next.java
@@ -16,11 +16,12 @@ import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.yalantis.ucrop.UCrop;
 
 public class After_next extends AppCompatActivity {
 
     ImageView imageView;
-    Uri camera_Image_Uri;
+    Uri camera_Image_Uri, uri_new;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +35,7 @@ public class After_next extends AppCompatActivity {
 
             // gallary_Image_Uri =in.getData();
             //imageView.setImageURI(gallary_Image_Uri);}
-        Intent in= getIntent();
+        final Intent in= getIntent();
         camera_Image_Uri=in.getData();
         imageView.setImageURI(camera_Image_Uri);
 
@@ -51,12 +52,23 @@ public class After_next extends AppCompatActivity {
                 return false;
             }
 
+            public void onActivityResult(int requestCode, int resultCode, Intent data) {
+                if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+                    final Uri resultUri = UCrop.getOutput(data);
+                } else if (resultCode == UCrop.RESULT_ERROR) {
+                    final Throwable cropError = UCrop.getError(data);
+                }
+            }
+
 
 
             private void UserItemSelected(MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.crop:
-
+                        UCrop.of(camera_Image_Uri,uri_new)
+                                .withAspectRatio(16, 9)
+                                .withMaxResultSize(100, 100)
+                                .start(After_next.this);
 
                         break;
                     case R.id.rotation:
