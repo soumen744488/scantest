e9fc04359f154291283a41969b89a08d4b8917bb
souvik <souvikghosh687@gmail.com>
2020-09-18 15:34:03 +0530

    filter dialogadded

--- app/src/main/java/com/example/scantest/After_next.java
+++ app/src/main/java/com/example/scantest/After_next.java
@@ -2,8 +2,10 @@ package com.example.scantest;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,9 +13,13 @@ import android.net.Uri;
 import android.opengl.Matrix;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -28,17 +34,23 @@ import com.yalantis.ucrop.UCrop;
 public class After_next extends AppCompatActivity {
 
     ImageView imageView;
-    Uri camera_Image_Uri, gallaryuri,uri_new;
+    Uri gallaryuri;
+    AlertDialog adjustdialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_next);
         imageView = findViewById(R.id.imageView2);
-
-        Intent in = getIntent();
+        
         gallaryuri = Uri.parse(getIntent().getStringExtra("imageUri"));
         imageView.setImageURI(gallaryuri);
+
+        AlertDialog.Builder builderadjust = new AlertDialog.Builder(After_next.this);
+        LayoutInflater infab = this.getLayoutInflater();
+        View aboutdialogView = infab.inflate(R.layout.filters,null);
+        builderadjust.setView(aboutdialogView);
+        adjustdialog=builderadjust.create();
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.edit_nav);
@@ -53,9 +65,14 @@ public class After_next extends AppCompatActivity {
                 switch (item.getItemId()) {
 
                     case R.id.filter:
+                        adjustdialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                        WindowManager.LayoutParams wlp = adjustdialog.getWindow().getAttributes();
+                        wlp.gravity= Gravity.BOTTOM ;
+                        adjustdialog.show();
+                        adjustdialog.setCancelable(false);
                         break;
 
-                    case R.id.color:
+                    case R.id.adjust:
                         break;
 
                     case R.id.trash:
